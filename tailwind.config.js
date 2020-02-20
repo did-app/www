8364798e9bc0cc530149d9398729a1fb5a147589
Peter Saxton <peterhsaxton@gmail.com>
2020-02-20 09:20:04 +0000

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          "500": "#00dfc0",
+          "600": "#00bba1",
+          "700": "#00b39a",
+          "900": "#004138"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
